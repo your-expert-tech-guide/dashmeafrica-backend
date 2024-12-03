8ad8890e4cea89eb609665e06f89ac0cec052dcf
unknown <Buzz-brain>
2024-12-04 00:19:15 +0100

    first commit

--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,6 +1,10 @@
+const express = require('express');
+const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 
 // Example: Protected Profile Route
 router.get('/profile', protect, async (req, res) => {
   res.json(req.user);
 });
+
+module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -44,29 +44,6 @@ router.post('/register', async (req, res) => {
 
 // @desc Authenticate user
 // @route POST /api/users/login
-// router.post('/login', async (req, res) => {
-//     const { email, password } = req.body;
-  
-//     if (!email || !password) {
-//       return res.status(400).json({ message: 'Please provide email and password' });
-//     }
-  
-//     try {
-//       const user = await User.findOne({ email });
-//       if (!user || !(await bcrypt.compare(password, user.password))) {
-//         return res.status(401).json({ message: 'Invalid email or password' });
-//       }
-  
-//       res.json({
-//         _id: user._id,
-//         name: user.name,
-//         email: user.email,
-//         token: generateToken(user._id),
-//       });
-//     } catch (error) {
-//       res.status(500).json({ message: error.message });
-//     }
-//   });
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
   
