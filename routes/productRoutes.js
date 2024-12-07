71bb916634eb09ded468fff5e4bd33abdc5a67a7
unknown <Buzz-brain>
2024-12-07 04:51:33 +0100

    first commit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -215,14 +215,6 @@ router.post('/donate', upload.single('image'), async (req, res) => {
 // @desc Get all products
 // @route GET /api/products
 // @access Public
-// router.get('/', async (req, res) => {
-//   try {
-//     const products = await Product.find();
-//     res.status(200).json(products);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Server Error', error: error.message });
-//   }
-// });
 
 router.get('/', async (req, res) => {
   try {
